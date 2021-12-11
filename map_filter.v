4ab9282c7e48177277b0cd58340ca16ef0fc1458
Takayuki Ogawa <takayuki@compusophia.com>
2021-12-11 09:40:35 +0900

    Added nodup_map_filter lemma.

--- map_filter.v
+++ map_filter.v
@@ -25,4 +25,28 @@ Section MapFilter.
         apply H0.
   Qed.
 
+  Lemma nodup_map_filter
+    : forall (A B : Type) (l : list A) (f1 : A -> bool) (f2 : A -> B),
+      NoDup (map f2 l) -> NoDup (map f2 (filter f1 l)).
+  Proof.
+    intros.
+    induction l.
+    - simpl; auto.
+    - simpl.
+      simpl in H.
+      rewrite NoDup_cons_iff in H.
+      destruct H.
+      remember (f1 a) as b; destruct b.
+      + simpl.
+        rewrite NoDup_cons_iff.
+        split.
+        apply not_in_map_not_in_map_filter.
+        apply H.
+        apply IHl.
+        apply H0.
+      + apply IHl.
+        simpl in H.
+        apply H0.
+  Qed.
+
 End MapFilter.
